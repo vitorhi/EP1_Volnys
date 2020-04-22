e9bf1ba21a6eae905e87a24bd483fe2e5fcdce80
vitorhi <vitorhi@msn.com>
2020-04-22 14:29:20 -0300

    Substuição por fila

--- prodcons.c
+++ prodcons.c
@@ -4,6 +4,10 @@
 // para compilar:  cc -o phtread pthread.c -lpthread
 
 #include <pthread.h>
+#include<stdio.h>
+
+#include "fila.h"
+
 
 #define  FALSE 0
 #define  TRUE  1
@@ -18,71 +22,74 @@ pthread_mutex_t  mutex;
 int buffer;
 int estado = BUFFERVAZIO;
 
+struct fila *queue;
+
 
 void produtor(int id)
-        {
+{
 	int i=0;
 	int item;
-        int aguardar;
+	int aguardar;
 
 	printf("Inicio produtor %d \n",id);
 	while (i < 10)
-	    {
-            //produzir item
-	    item = i + (id*1000);
+	{
+        //produzir item
+		item = i + (id*1000);
 
-            do
-	       {
-             pthread_mutex_lock(&mutex);
-	        aguardar = FALSE;
-	        if (estado == BUFFERCHEIO)
-		      {
-		      aguardar = TRUE;
-	            pthread_mutex_unlock(&mutex);
-		      }
-		   } while (aguardar == TRUE);
+		do
+		{
+			pthread_mutex_lock(&mutex);
+			aguardar = FALSE;
+			if (FilaCheia( &queue))
+			{
+				aguardar = TRUE;
+				pthread_mutex_unlock(&mutex);
+			}
+		} while (aguardar == TRUE);
 
 	    //inserir item
-          printf("Produtor %d inserindo item %d\n", id, item); 
-          buffer = item;
-	    estado = BUFFERCHEIO;
-	    
-	    pthread_mutex_unlock(&mutex);
-	    i++;
-	    sleep(2);
-	    }
+		printf("Produtor %d inserindo item %d\n", id, item); 
+		// buffer = item;
+		// estado = BUFFERCHEIO;
+		InserirFila(&queue, item); 
+		pthread_mutex_unlock(&mutex);
+		i++;
+		sleep(2);
+	}
 	printf("Produtor %d terminado \n", id); 
-        }
+}
 
 void consumidor(int id)
-        {
+{
 	int item;
 	int aguardar;
 
 	printf("Inicio consumidor %d \n",id);
 	while (1)
-	    {
+	{
             // retirar item da fila
-            do
+		do
 		{
-                pthread_mutex_lock(&mutex);
-	        aguardar = FALSE;
-	        if (estado == BUFFERVAZIO)
-		    {
-	            aguardar = TRUE;
-		    pthread_mutex_unlock(&mutex);
-		    }
-	        } while (aguardar == TRUE);
-	    item = buffer;
-	    estado = BUFFERVAZIO;
-          printf("Consumidor %d consumiu item %d\n", id, item); 
-	    pthread_mutex_unlock(&mutex);
+			pthread_mutex_lock(&mutex);
+			aguardar = FALSE;
+			if ( FilaVazia(&queue))
+			{
+				aguardar = TRUE;
+				pthread_mutex_unlock(&mutex);
+			}
+		} while (aguardar == TRUE);
+		// item = buffer;
+		// estado = BUFFERVAZIO;
+		item = RetirarFila(&queue);
+		printf("Consumidor %d consumiu item %d\n", id, item); 
+		pthread_mutex_unlock(&mutex);
 
 	    // processar item
- 	    sleep(2);
-	    }
+		sleep(2);
+	}
 	printf("Consumidor %d terminado \n", id); 
-        }
+}
 
 int main()
 { 
@@ -95,14 +102,15 @@ int main()
 	printf("Programa Produtor-Consumidor\n");
 
 	printf("Iniciando variaveis de sincronizacao.\n");
+	InitFila(&queue);
 	pthread_mutex_init(&mutex,NULL);
 
-     printf("Disparando threads produtores\n");
+	printf("Disparando threads produtores\n");
 	pthread_create(&prod1, NULL, (void*) produtor,1);
 	pthread_create(&prod2, NULL, (void*) produtor,2);
 	pthread_create(&prod3, NULL, (void*) produtor,3);
 
-        printf("Disparando threads consumidores\n");
+	printf("Disparando threads consumidores\n");
 	pthread_create(&cons1, NULL, (void*) consumidor,1);
 	pthread_create(&cons2, NULL, (void*) consumidor,2);
 
@@ -112,6 +120,6 @@ int main()
 	pthread_join(cons1,NULL);
 	pthread_join(cons2,NULL);
 	
-        printf("Terminado processo Produtor-Consumidor.\n\n");
+	printf("Terminado processo Produtor-Consumidor.\n\n");
 }
 
